93b6fea8412214a107ad98f07d1d9a865c253002
Zoeyunnnn <hicandy0406@gmail.com>
2019-10-25 00:44:21 +0800

    First commit

---
+++ src/Zoee.java
@@ -0,0 +1,142 @@
+import java.util.*;
+public class Zoee {
+    public static void main(String[] args) {
+
+//        Scanner sc = new Scanner(System.in);
+//        int n = sc.nextInt();
+//
+//        int sum = 0,i;
+//        for (i=3;i<=n;i+=3){
+//            sum += i;
+//        }
+//        System.out.println(sum);
+
+//        Scanner sc = new Scanner(System.in);
+//        int n = sc.nextInt();
+//
+//        int m = n/2;
+//        boolean ha = true;
+//        for (int i = 2;i<=m;i++){
+//            if (n%i==0){
+//                ha = false;
+//                break;
+//            }
+//        }if (ha){
+//            System.out.println("Yes");
+//        }else {
+//            System.out.println("No");
+//        }
+
+//        Scanner sc = new Scanner(System.in);
+//        int n = sc.nextInt();
+//
+//        int sum=0,i;
+//        for (i=1;i<=n;i++){
+//            sum += i*(i+1);
+//        }
+//        System.out.println(sum);
+
+//        Scanner sc = new Scanner(System.in);
+//        float n = sc.nextFloat();
+//        float max = n,min = n;
+//
+//        for (int i=1;i<=9;i++){
+//            n = sc.nextFloat();
+//            if (max<n){
+//                max = n;
+//            }else if (min>n){
+//                min = n;
+//            }
+//        }
+//        System.out.printf("%.2f\n",max);
+//        System.out.printf("%.2f\n",min);
+
+//        Scanner sc = new Scanner(System.in);
+//        int n = sc.nextInt();
+//
+//        boolean ha = true;
+//        int sum = 0,ct;
+//        for (int i=2;i<=n;i++){
+//            ct = i;
+//            for (;ct!=1;){
+//                if (ct%2==0){
+//                    ct /= 2;
+//                }else if (ct%3==0){
+//                    ct /= 3;
+//                }else if (ct%5==0){
+//                    ct /= 5;
+//                }else {
+//                    ha = false;
+//                    break;
+//                }
+//            }if (ha)sum+=i;
+//            ha = true;
+//        }
+//        System.out.println(sum);
+
+//    Scanner sc = new Scanner(System.in);
+//    int n = sc.nextInt();
+//
+//    int sum=0,i=1,j=1;
+//    for (i=1;i<=n;i++){
+//        for (j=1;j<n;j++){
+//            sum = i*j;
+//            System.out.print(sum+"\t");
+//        }
+//        System.out.println(i*(j));
+//    }
+
+//        Scanner sc = new Scanner(System.in);
+//        int n = sc.nextInt();
+//
+//        long sum = 1;       /*用int會爆*/
+//        for (int i=1;i<=n;i++){
+//            sum *= i;
+//        }
+//        System.out.println(sum);
+
+//        Scanner sc = new Scanner(System.in);
+//        int n = sc.nextInt();
+//
+//        double sum = 0;
+//        for (int i=1;i<=n;i++){
+//            sum = sum+1d/((2*i-1)*(2*i));
+//        }
+//        System.out.println(sum);
+
+//        Scanner sc = new Scanner(System.in);
+//        int n = sc.nextInt();
+//
+//        int sum = 0;
+//        for (int i=1;i<=n;i++){
+//            if (n%i==0){
+//                sum = i;
+//                System.out.println(i);
+//            }
+//        }
+
+
+//        Scanner sc = new Scanner(System.in);
+//        int n = sc.nextInt();
+//        char c = sc.next().charAt(0);
+//
+//        char sum=c;
+//        for (int i=0;i<n;i++){
+//            for (int j=0;j<n;j++){
+//                System.out.print(c);
+//            }
+//            System.out.println();
+//        }
+
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        int sum = 0,sun = 1;
+        for (int i=1;i<=n;i++){
+                sun *= 2;
+                sum += sun;
+        }
+        System.out.println(sum);
+    }
+}
+
